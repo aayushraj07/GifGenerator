03b2f0e9ede5bcd52120259fba98d65336950c4f
aayushraj07 <aayush.97raj@gmail.com>
2018-12-08 20:52:30 +0530

    Search's class component made and state initalised ,which is taking all the input in it .

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ class App extends Component {
     return (
       <div>
         {" "}
-        <h1>Best MemeGenrator</h1>;
-        <Search />;
+        <h1>Best MemeGenrator</h1>
+        <Search />
       </div>
     );
   }
 }
 
 export default App;
+
+//Watch video after 14. APi installed. in youtube app video
--- src/search.js
+++ src/search.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Search = () => {
-  return <input />;
-};
+// const Search = () => {
+//   return <input />;
+// };
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+        {/* Value of the input : {this.state.term} */}
+      </div>
+    );
+  }
+}
 
 export default Search;
