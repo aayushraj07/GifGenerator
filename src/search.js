f585dbf9eb460e506476bb1de6b2300d97dd1aeb
aayushraj07 <aayush.97raj@gmail.com>
2018-12-09 12:11:38 +0530

    axios not working

--- src/search.js
+++ src/search.js
@@ -1,22 +1,36 @@
 import React, { Component } from "react";
 import Display from "./display";
+import axios from "axios";
 
 // const Search = () => {
 //   return <input />;
 // };
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { term: "" };
+  state = {
+    gifUrl: "",
+    keyword: "pokemon",
+    giphyApiKey: "wrSXrhvnmsHgkOlpWT5iE7kJbmxxInFG",
+    rating: "G"
+  };
+  componentDidMount() {
+    axios
+      .get(
+        `http://api.giphy.com/v1/gifs/search?q=${
+          this.state.keyword
+        }&api_key=wrSXrhvnmsHgkOlpWT5iE7kJbmxxInFG`
+      )
+      .then((response: { data: { data: { image_url: string } } }) => {
+        // console.log(response);
+        this.setState({ gifUrl: response.data.data.image_url });
+      });
   }
 
   render() {
     return (
       <div>
         <input
-          value={this.state.term}
+          value={this.state.keyword}
           onChange={event => this.setState({ term: event.target.value })}
         />
       </div>
